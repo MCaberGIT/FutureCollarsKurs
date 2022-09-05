990d45bbb84e511ad3e4daf1b1b1bb659e0f9c37
Michal Caber <javacaber@gmail.com>
2022-09-05 19:29:31 +0200

    zadanie 5 lekcja 7

---
+++ src/main/java/com/featurecollars/lesson7/zad5/RightTriangleChecker.java
@@ -0,0 +1,7 @@
+package com.featurecollars.lesson7.zad5;
+
+public class RightTriangleChecker {
+    public static boolean isRightTriangle(int a, int b, int c) {
+        return a * a + b * b == c * c;
+    }
+}
