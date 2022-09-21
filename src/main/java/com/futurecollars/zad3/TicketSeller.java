8e966d6f0049ae84dd45ac8b0b00816b5ac6c41c
Michal Caber <javacaber@gmail.com>
2022-09-21 21:53:12 +0200

    zmainy zadanie numer 3 lekcja 7

--- src/main/java/com/futurecollars/zad3/TicketSeller.java
+++
@@ -1,7 +0,0 @@
-package com.futurecollars.zad3;
-
-public class TicketSeller extends BaseEmployee {
-    public TicketSeller(String name, String lastname, int yearOfJoining) {
-        super(name, lastname, yearOfJoining);
-    }
-}
