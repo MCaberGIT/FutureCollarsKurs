8fed85f306444131b6b505e751278baf58f6e4b1
Michal Caber <javacaber@gmail.com>
2022-09-22 02:06:15 +0200

    zadanie numer 9 lekcja 8

--- src/main/java/com/futurecollars/Triangle.java
+++ src/main/java/com/futurecollars/Triangle.java
@@ -1,21 +1,19 @@
 package com.futurecollars;
 
 public class Triangle {
-    public class Triangle {
-        private final int width;
-        private final int height;
+    private final int width;
+    private final int height;
 
-        public Triangle(int width, int height) {
-            this.width = width;
-            this.height = height;
-        }
+    public Triangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
 
-        @Override
-        public String toString() {
-            return "Triangle{" +
-                    "width=" + width +
-                    ", height=" + height +
-                    '}';
-        }
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
     }
 }
